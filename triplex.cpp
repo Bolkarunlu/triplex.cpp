a74c7cfd0354dbed0ec82f6c12cb8ae07fe429b0
Bolkar1999 <bolkarunlu77@gmail.com>
2022-03-04 14:07:24 +0300

    4.03.2022

---
+++ triplex.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main () 
+{ 
+   std::cout << "HELLO WOLRD";
+   std::cout << std::endl;
+   std::cout << " Bolkar en iyi yazılımcıdır";
+  
+  int a = 4;
+  int b = 3;
+  int c = 2;
+  
+  int sum = a  + b + c ;
+  int product = a * b * c ;
+
+  
+  std::cout << std::endl;
+  std::cout << sum << std::endl;
+  std::cout << product << std::endl;
+  
+  
+  
+  
+  
+  
+  
+  
+   return 0;
+}
